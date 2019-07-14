7123cad3e626b7f349ecc932de7d9f9b2694ffe4
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-14 17:49:28 +0530

    code update

---
+++ Patterns/Pattern2.cpp
@@ -0,0 +1,22 @@
+//Author name-Pankaj jaiswal
+//Date-14-07-2019
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+*/
+
+#include<iostream>
+using namespace std;
+		int main(){
+		int i,j,n;
+		cin>>n;
+		for(i=1;i<=n;i++){
+			for(j=1;j<=i;j++){
+			cout<<j<<"\t";
+			}
+			cout<<endl;
+		}
+		return 0;
+		}
