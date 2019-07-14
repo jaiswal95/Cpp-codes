27396749b6f4d5afb1cb0906cf063d62eb1fdb42
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-14 16:39:45 +0530

    code update

---
+++ Patterns/Floyd'sTriangle.cpp
@@ -0,0 +1,22 @@
+//Author-Pankaj jaiswal
+//Date-14-07-2019
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n,k=1;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<k<<"\t";
+			k++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
