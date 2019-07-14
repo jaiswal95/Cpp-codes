42f09f94ec0e99f42b7d20f22e14e0740245da7c
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-14 23:28:34 +0530

    code update

---
+++ Patterns/Pattern9.cpp
@@ -0,0 +1,29 @@
+// Author- Pankaj jaiswal
+// Date- 14-07-2019
+
+/* 
+1 * * * *
+1 2 * * *
+1 2 3 * *
+1 2 3 4 *
+1 2 3 4 5
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+		for(int i=1;i<=n;i++){
+		 	for(int j=1;j<=n;j++){
+		 			if(j>i){
+		 			cout<<"*"<<" ";
+		 			}
+		 			else{
+		 			cout<<j<<" ";
+		 			}
+		 	}
+		 	cout<<endl;
+		}
+		return 0;
+}
