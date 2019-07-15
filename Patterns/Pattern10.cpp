f60dc4602ccef41c3484c483f06528e9c080f0a5
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-15 12:05:58 +0530

    code update

---
+++ Patterns/Pattern10.cpp
@@ -0,0 +1,26 @@
+// Author- Pankaj jaiswal
+// Date- 14-07-2019
+/*
+1 1 1 1 1
+0 1 1 1 1
+0 0 1 1 1
+0 0 0 1 1
+0 0 0 0 1
+*/
+
+#include<iostream>
+using namespace std;
+		int main(){
+			int n;
+			cin>>n;
+				for(int i=1;i<=n;i++){
+						for(int j=1;j<=n;j++){
+						 		if(j>i||i==j)
+						 		   cout<<"1"<<"\t";
+						 		else
+						 			cout<<"0"<<"\t";
+						}
+					cout<<endl;	
+				}
+				return 0;
+		}
