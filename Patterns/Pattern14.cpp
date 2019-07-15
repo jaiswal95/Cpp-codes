64f6c72302d153c4f8e4ff3467bd380d99c3e0b1
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-15 18:30:06 +0530

    code update

---
+++ Patterns/Pattern14.cpp
@@ -0,0 +1,28 @@
+//Author- Pankaj jaiswal
+//Date- 15-07-2019
+/*
+* * * 1
+* * 2 1
+* 3 2 1
+4 3 2 1
+*/
+#include<iostream>
+using namespace std;
+int main(){
+		int n,s;
+		cin>>n;
+		s=n;
+	for(int i=1;i<=n;i++){
+			s=s-i;
+		for(int j=i;j>=1;j--){
+			while(s>0){
+				cout<<"*"<<"\t";
+				s--;
+				}
+			cout<<j<<"\t";
+		}
+		cout<<endl;
+		s=n;
+	}
+	return 0;
+}
