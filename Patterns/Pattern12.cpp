fa816b3d37997f764616f4476aa925665acd64e3
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-15 14:29:01 +0530

    code update

---
+++ Patterns/Pattern12.cpp
@@ -0,0 +1,28 @@
+//Author- Pankaj jaiswal
+//Date- 14-07-2019
+
+/*
+1 
+1 1
+1 1 2
+1 1 2 2
+1 1 2 2 3
+*/
+
+#include<iostream>
+using namespace std;
+		int main(){
+		int k,n;
+		cin>>n;
+			for(int i=1;i<=n;i++){
+							k=1;
+					for(int j=1;j<=i;j++){
+							cout<<k<<"\t";
+								if(j%2==0){
+									k++;
+								}
+                            }
+                            cout<<endl;
+			}
+			return 0;
+		}
