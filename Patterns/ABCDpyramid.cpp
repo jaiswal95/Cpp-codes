8ff78f021a919bc6c16e308f5bd7a201ab31a93c
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-17 16:43:51 +0530

    code update

---
+++ Patterns/ABCDpyramid.cpp
@@ -0,0 +1,23 @@
+//Author- Pankaj jaiswal
+//Date- 17-07-2019
+
+#include<iostream>
+using namespace std;
+	int main(){
+			int n;
+			cin>>n;
+		  for(int i=0;i<n;i++){
+		  		char ch='A';
+		  	 for(int j=1;j<=n-i;j++){
+		  	 		cout<<ch;
+		  	 		ch=ch+1;
+		  	 }
+		  	 ch=ch-1;
+		  	 for(int j=1;j<=n-i;j++){
+		  	 		cout<<ch;
+		  	 		ch=ch-1;
+		  	 }
+		  	 cout<<endl;
+		  }
+		  return 0;
+	}
