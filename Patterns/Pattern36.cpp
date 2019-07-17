36134ad9bf6acdbd77b09d56f97db98ca85cddf8
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-17 13:51:50 +0530

    code update

---
+++ Patterns/Pattern36.cpp
@@ -0,0 +1,27 @@
+//Author- Pankaj jaiswal
+//Date- 17-07-2019
+/*
+1
+22
+303
+4004
+50005
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			if(i==j||j==1){
+				cout<<i;
+			}
+			else{
+				cout<<0;
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
