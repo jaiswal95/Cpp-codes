5e3c2898d8eb50122a6e037a8d35916751b6c3cb
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-18 15:46:53 +0530

    code update

---
+++ Patterns/Pattern42.cpp
@@ -0,0 +1,27 @@
+//Author- Pankaj jaiswal
+//Date- 18-07-2019
+/*
+*   * * *
+*   *
+* * * * *
+    *   *
+* * *   *
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int mid=(n+1)/2;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if((i==1&&j>=mid)||(j==1&&i<=mid)||(j==n&&i>=mid)||(i==n&&j<=mid)||(i==mid||j==mid)){
+				cout<<"* ";
+			}
+			else{
+				cout<<"  ";
+			}
+		}
+		cout<<endl;
+	}
+}
