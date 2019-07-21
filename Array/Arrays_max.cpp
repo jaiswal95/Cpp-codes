dc84a6a19aa36800071909ff0c8988158d26616e
jaiswalsahab <pankaj1995jaiswal@gmail.com>
2019-07-21 23:26:44 +0530

    codeupdate

---
+++ Array/Arrays_max.cpp
@@ -0,0 +1,28 @@
+//Author : Pankaj jaiswal
+//Date : 21th July 2019
+//Arrays-Max Value In array
+/*
+Inputs
+5
+1 2 3 4 5
+output 
+5
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(max<=arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    cout<<max<<endl;
+}
